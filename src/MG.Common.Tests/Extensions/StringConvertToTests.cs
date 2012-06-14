1bbd8901bbf7eda49f24818071ea294f9a53abbb
Mike Grabski <me@mikegrabski.com>
2012-06-14 03:05:09 -0400

    Fixed string.ConvertTo extension, added unit tests for essential types, added support for converting enums, Base64 and hex encoded strings to byte arrays.

---
+++ src/MG.Common.Tests/Extensions/StringConvertToTests.cs
@@ -0,0 +1,112 @@
+﻿
+
+using System;
+using System.Globalization;
+
+using NUnit.Framework;
+
+using FluentAssertions;
+
+using Moq;
+
+namespace MG.Extensions
+{
+    [TestFixture]
+    public class StringConvertToTests
+    {
+        #region Setup/Teardown for fixture
+
+        [TestFixtureSetUp]
+        public void SetUpFixture()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TearDownFixture()
+        {
+        }
+
+        #endregion
+
+        #region Setup/Teardown for each test
+
+        [SetUp]
+        public void SetUpTest()
+        {
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+
+        #endregion
+
+        [Test]
+        public void Should_convert_numeric_types()
+        {
+            byte.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<byte>().Should().Be(byte.MaxValue);
+            short.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<short>().Should().Be(short.MaxValue);
+            ushort.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<ushort>().Should().Be(ushort.MaxValue);
+            int.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<int>().Should().Be(int.MaxValue);
+            uint.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<uint>().Should().Be(uint.MaxValue);
+            long.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<long>().Should().Be(long.MaxValue);
+            ulong.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<ulong>().Should().Be(ulong.MaxValue);
+            decimal.MaxValue.ToString(CultureInfo.InvariantCulture).ConvertTo<decimal>().Should().Be(decimal.MaxValue);
+        }
+
+        [Test]
+        public void Should_convert_booleans()
+        {
+            "false".ConvertTo(true).Should().BeFalse();
+            "FALSE".ConvertTo(true).Should().BeFalse();
+            "False".ConvertTo(true).Should().BeFalse();
+            "true".ConvertTo<bool>().Should().BeTrue();
+            "TRUE".ConvertTo<bool>().Should().BeTrue();
+            "True".ConvertTo<bool>().Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_convert_enum_names()
+        {
+            "sunday".ConvertTo(DayOfWeek.Tuesday).Should().Be(DayOfWeek.Sunday);
+            "Sunday".ConvertTo(DayOfWeek.Tuesday).Should().Be(DayOfWeek.Sunday);
+            "SUNDAY".ConvertTo(DayOfWeek.Tuesday).Should().Be(DayOfWeek.Sunday);
+        }
+
+        [Test]
+        public void Should_convert_datetimes()
+        {
+            "6/10/2012 1:15 PM".ConvertTo<DateTime>(provider: CultureInfo.InvariantCulture)
+                .Should().Be(new DateTime(2012, 6, 10, 13, 15, 0));
+            "6/10/2012".ConvertTo<DateTime>(provider: CultureInfo.InvariantCulture)
+                .Should().Be(new DateTime(2012, 6, 10));
+        }
+
+        [Test]
+        public void Should_convert_guid()
+        {
+            // Arrange
+            var guid = Guid.NewGuid();
+
+            // Act / Assert
+            guid.ToString().ConvertTo<Guid>().Should().Be(guid);
+            guid.ToString("N").ConvertTo<Guid>().Should().Be(guid);
+            guid.ToString("B").ConvertTo<Guid>().Should().Be(guid);
+            guid.ToString("P").ConvertTo<Guid>().Should().Be(guid);
+            guid.ToString("X").ConvertTo<Guid>().Should().Be(guid);
+        }
+
+        [Test]
+        public void Should_convert_byte_array()
+        {
+            // Arrange
+            var expected = new byte[] {10, 24, 100, 23, 93, 45, 200};
+
+            // Act / Assert
+            Convert.ToBase64String(expected).ConvertTo<byte[]>().Should().Equal(expected);
+            expected.ToHexString().ConvertTo<byte[]>().Should().Equal(expected);
+
+        }
+    }
+}
--- src/MG.Common/Extensions/StringExtensions.ConvertTo.cs
+++ src/MG.Common/Extensions/StringExtensions.ConvertTo.cs
@@ -17,7 +17,9 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace MG.Extensions
 {
@@ -34,11 +36,14 @@ namespace MG.Extensions
         /// <param name="defaultValue">The default value that should be returned if conversion fails.</param>
         /// <param name="provider">The format provider.</param>
         /// <returns>The value of the string converted to the desired type, or the default value if conversion failed.</returns>
-        public static T ConvertTo<T>(string value, T defaultValue = default(T), IFormatProvider provider = null)
+        public static T ConvertTo<T>(this string value, T defaultValue = default(T), IFormatProvider provider = null)
         {
             try
             {
                 if (typeof(T) == typeof(Guid)) return (T) (object) (new Guid(value));
+                if (typeof(T).IsEnum) return (T) Enum.Parse(typeof (T), value, true);
+                if (typeof(T) == typeof(byte[]))
+                    return (T) (object) ConvertToByteArray(value, (byte[]) (object) defaultValue, provider);
 
                 return (T) Convert.ChangeType(value, typeof (T), provider ?? CultureInfo.CurrentCulture);
             }
@@ -46,6 +51,71 @@ namespace MG.Extensions
             {
                 return defaultValue;
             }
+        }
+
+        private static byte[] ConvertToByteArray(string value, byte[] defaultValue, IFormatProvider provider)
+        {
+            if (IsPossiblyHexEncoded(value)) return ConvertHexEncodedString(value, defaultValue, provider);
+            if (IsPossiblyBase64Encoded(value)) return ConvertBase64EncodedString(value, defaultValue, provider);
+
+            return defaultValue;
+        }
+
+        private static byte[] ConvertHexEncodedString(string value, byte[] defaultValue, IFormatProvider provider)
+        {
+            var list = new List<byte>();
+
+            for (int i = 0; i < value.Length; i = i+2)
+            {
+                byte b;
+                var hex = value.Substring(i, 2);
+
+                if (!byte.TryParse(hex, NumberStyles.HexNumber, provider, out b)) return defaultValue;
+
+                list.Add(b);
+            }
+
+            return list.ToArray();
+        }
+
+        private static byte[] ConvertBase64EncodedString(string value, byte[] defaultValue, IFormatProvider provider)
+        {
+            try
+            {
+                return Convert.FromBase64String(value);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        private static readonly HashSet<char> Base64Characters = new HashSet<char> { 
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
+                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+                'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', 
+                '='
+            };
+
+        private static readonly HashSet<char> HexCharacters = new HashSet<char>
+                                                                  {
+                                                                      'A', 'B', 'C', 'D', 'E', 'F',
+                                                                      '0', '1', '2', '3', '4', '5',
+                                                                      '6', '7', '8', '9', 'a', 'b',
+                                                                      'c', 'd', 'e', 'f'
+                                                                  }; 
+
+        private static bool IsPossiblyBase64Encoded(string value)
+        {
+            return !string.IsNullOrEmpty(value) &&  value.All(c => Base64Characters.Contains(c));
+        }
+
+        private static bool IsPossiblyHexEncoded(string value)
+        {
+            return !string.IsNullOrEmpty(value) 
+                && value.Length%2 == 0 
+                && value.All(c => HexCharacters.Contains(c));
         }
     }
 }
