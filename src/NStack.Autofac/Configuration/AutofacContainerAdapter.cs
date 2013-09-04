608d761a98d64ec15e3d519aaf7b15710333fd0d
Mike Grabski <mike@workflow.com>
2013-09-03 23:30:24 -0400

    Added NStack.Autofac

---
+++ src/NStack.Autofac/Configuration/AutofacContainerAdapter.cs
@@ -0,0 +1,204 @@
+﻿#region header
+// <copyright file="AutofacContainerAdapter.cs" company="mikegrabski.com">
+//    Copyright 2013 Mike Grabski
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+#endregion
+
+using System;
+
+using Autofac;
+
+namespace NStack.Configuration
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IContainerAdapter"/> that integrates with Autofac.
+    /// </summary>
+    public class AutofacContainerAdapter : IContainerAdapter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public AutofacContainerAdapter(ContainerBuilder builder)
+        {
+            Builder = builder;
+        }
+
+        /// <summary>
+        /// Gets the container builder where configuration is stored.
+        /// </summary>
+        protected ContainerBuilder Builder { get; private set; }
+
+        #region Implementation of IContainerAdapter
+
+        /// <summary>
+        /// Registers an implementation of a service using an @delegate in the default scope.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="delegate">The @delegate that will be invoked when the service needs to be resolved.</param>
+        /// <param name="name">Optional name of hte service implementation.</param>
+        public void Register<TService, TImplementation>(Func<IResolver, TImplementation> @delegate, string name = null)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Builder.Register(c => @delegate(new AutofacResolver(c)))
+                    .As<TService>()
+                    .InstancePerLifetimeScope();
+            }
+            else
+            {
+                Builder.Register(c => @delegate(new AutofacResolver(c)))
+                       .Named<TService>(name)
+                       .InstancePerLifetimeScope();
+            }
+        }
+
+        /// <summary>
+        /// Registers an implementation type of the specified service type in the default scope.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="name">The name of the service implementation.</param>
+        public void Register<TService, TImplementation>(string name = null) where TImplementation : TService
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Builder.RegisterType<TImplementation>()
+                    .As<TService>()
+                    .InstancePerLifetimeScope();
+            }
+            else
+            {
+                Builder.RegisterType<TImplementation>()
+                    .Named<TService>(name)
+                    .InstancePerLifetimeScope();
+            }
+        }
+
+        /// <summary>
+        /// Registers a generic implementation type of the specified generic service type in the default scope.
+        /// </summary>
+        /// <param name="service">The generic type of the service.</param>
+        /// <param name="implementation">The generic type of the implementation.</param>
+        /// <param name="name">The name of the service implementation.</param>
+        public void RegisterGeneric(Type service, Type implementation, string name = null)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Builder.RegisterGeneric(implementation)
+                    .As(service)
+                    .InstancePerLifetimeScope();
+            }
+            else
+            {
+                Builder.RegisterGeneric(implementation)
+                    .Named(name, service)
+                    .InstancePerLifetimeScope();
+            }
+        }
+
+        /// <summary>
+        /// Registers a single instance of the implementation type for the specified service type.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="name">The name of the service implementation.</param>
+        public void RegisterSingleInstance<TService, TImplementation>(string name = null) where TImplementation : TService
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Builder.RegisterType<TImplementation>()
+                    .As<TService>()
+                    .SingleInstance();
+            }
+            else
+            {
+                Builder.RegisterType<TImplementation>()
+                    .Named<TService>(name)
+                    .SingleInstance();
+            }
+        }
+
+        /// <summary>
+        /// Registers a single instance of an instance as the specified type.
+        /// </summary>
+        /// <param name="type">The type the instance should be registered as.</param>
+        /// <param name="instance">The service instance.</param>
+        /// <param name="name">The name of the service instance.</param>
+        public void RegisterSingleInstance(Type type, object instance, string name = null)
+        {
+             if (!string.IsNullOrEmpty(name))
+             {
+                 Builder.RegisterInstance(instance)
+                        .As(type)
+                        .SingleInstance();
+             }
+             else
+             {
+                 Builder.RegisterInstance(instance)
+                        .Named(name, type)
+                        .SingleInstance();
+             }
+        }
+
+        /// <summary>
+        /// Registers an instance of the specified service type as the single instance.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="instance">The service instance.</param>
+        /// <param name="name">Optional name of the service implementation.</param>
+        public void RegisterSingleInstance<TService, TImplementation>(TImplementation instance, string name = null) where TImplementation : class, TService
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Builder.RegisterInstance(instance)
+                    .As<TService>()
+                    .SingleInstance();
+            }
+            else
+            {
+                Builder.RegisterInstance(instance)
+                    .Named<TService>(name)
+                    .SingleInstance();
+            }
+        }
+
+        /// <summary>
+        /// Registers a single instance of the implementation type for the specified service type, where the implementation is constructed using the specified delegate.
+        /// </summary>
+        /// <typeparam name="TService">The service type.</typeparam>
+        /// <typeparam name="TImplementation">The implementation type.</typeparam>
+        /// <param name="delegate">The delegate invoked to construct the implementation.</param>
+        /// <param name="name">The name of the service implementation.</param>
+        public void RegisterSingleInstance<TService, TImplementation>(Func<IResolver, TImplementation> @delegate, string name = null) where TImplementation : TService
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Builder.Register(context => @delegate(new AutofacResolver(context)))
+                     .As<TService>()
+                     .SingleInstance();
+            }
+            else
+            {
+                Builder.Register(context => @delegate(new AutofacResolver(context)))
+                    .Named<TService>(name)
+                    .SingleInstance();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/NStack.Autofac/Configuration/AutofacResolver.cs
@@ -0,0 +1,86 @@
+﻿#region header
+// <copyright file="AutofacResolver.cs" company="mikegrabski.com">
+//    Copyright 2013 Mike Grabski
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+#endregion
+
+using System;
+
+using Autofac;
+
+namespace NStack.Configuration
+{
+    /// <summary>
+    /// A wrapper for Autofac <see cref="IComponentContext"/> which can be used during component registration in an adapter.
+    /// </summary>
+    internal class AutofacResolver : IResolver
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public AutofacResolver(IComponentContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        /// Gets the Autofac <see cref="IComponentContext"/>.
+        /// </summary>
+        protected IComponentContext Context { get; private set; }
+
+        #region Implementation of IResolver
+
+        /// <summary>
+        /// Resolves the specified service.
+        /// </summary>
+        /// <typeparam name="T">The type of the service to resolve.</typeparam>
+        /// <param name="name">The name of the service.</param>
+        /// <returns>The resolved service instance.</returns>
+        public T Get<T>(string name = null)
+        {
+            if (!string.IsNullOrEmpty(name)) return Context.ResolveNamed<T>(name);
+
+            return Context.Resolve<T>();
+        }
+
+        /// <summary>
+        /// Resolves the specified service.
+        /// </summary>
+        /// <param name="type">The type of the service to resolve.</param>
+        /// <param name="name">The name of the service.</param>
+        /// <returns>The resolved service instance.</returns>
+        public object Get(Type type, string name = null)
+        {
+            if (!string.IsNullOrEmpty(name)) return Context.ResolveNamed(name, type);
+
+            return Context.Resolve(type);
+        }
+
+        /// <summary>
+        /// Returns whether or not the specified service is registered.
+        /// </summary>
+        /// <typeparam name="T">The type of the service.</typeparam>
+        /// <param name="name">The name of the service.</param>
+        /// <returns>True if the service has been registered; otherwise, false.</returns>
+        public bool IsRegistered<T>(string name = null)
+        {
+            if (!string.IsNullOrEmpty(name)) return Context.IsRegisteredWithName<T>(name);
+
+            return Context.IsRegistered<T>();
+        }
+
+        #endregion
+    }
+}
