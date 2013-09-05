a25d5e20112e2851eaa7c1a60a2abad39a7dc66d
Mike Grabski <mike@workflow.com>
2013-09-04 21:30:07 -0400

    I should stop working from externals

--- src/NStack.Common/Configuration/IConfigurationEnvironment.cs
+++ src/NStack.Common/Configuration/IConfigurationEnvironment.cs
@@ -1,25 +1,38 @@
 ﻿#region header
-// -----------------------------------------------------------------------
-//  <copyright file="IConfigurationEnvironment.cs" company="Family Bronze, LTD">
-//      © 2013 Mike Grabski and Family Bronze, LTD All rights reserved.
-//  </copyright>
-// -----------------------------------------------------------------------
+
+// <copyright file="IConfigurationEnvironment.cs" company="mikegrabski.com">
+//    Copyright 2013 Mike Grabski
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+
 #endregion
+
 namespace NStack.Configuration
 {
     /// <summary>
-    /// A contract for objects that expose properties of the configuration environment.
+    ///     A contract for objects that expose properties of the configuration environment.
     /// </summary>
     public interface IConfigurationEnvironment
     {
         /// <summary>
-        /// Gets whether or not debugging is enabled.
+        ///     Gets whether or not debugging is enabled.
         /// </summary>
         bool DebuggingEnabled { get; }
 
         /// <summary>
-        /// Gets whether or not testing is enabled.
+        ///     Gets whether or not testing is enabled.
         /// </summary>
-        bool TestingEnabled { get; } 
+        bool TestingEnabled { get; }
     }
 }
--- src/NStack.Web/Extensions/ConfigurationExtensions.cs
+++ src/NStack.Web/Extensions/ConfigurationExtensions.cs
@@ -1,9 +1,21 @@
 ﻿#region header
-// -----------------------------------------------------------------------
-//  <copyright file="ConfigurationExtensions.cs" company="Family Bronze, LTD">
-//      © 2013 Mike Grabski and Family Bronze, LTD All rights reserved.
-//  </copyright>
-// -----------------------------------------------------------------------
+
+// <copyright file="ConfigurationExtensions.cs" company="mikegrabski.com">
+//    Copyright 2013 Mike Grabski
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+
 #endregion
 
 using System.Configuration;
@@ -13,15 +25,16 @@ using NStack.Configuration;
 namespace NStack.Extensions
 {
     /// <summary>
-    /// Utility extensions for NStack.Configuration.
+    ///     Utility extensions for NStack.Configuration.
     /// </summary>
     public static class ConfigurationExtensions
     {
         private const string ConfigTestingKey = "NStack:Testing";
+
         private const string ConfigDebuggingKey = "NStack:Debugging";
 
         /// <summary>
-        /// Reads environmental parameters from the app or web.config appKeys section.
+        ///     Reads environmental parameters from the app or web.config appKeys section.
         /// </summary>
         /// <typeparam name="T">The configuration type.</typeparam>
         /// <param name="configuration">The configuration.</param>
