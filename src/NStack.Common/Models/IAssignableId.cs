7cac3e72c307271154437e3ad8c0f2de58503816
Mike Grabski <mike@workflow.com>
2013-09-11 08:52:01 -0400

    Add IAssignableId

---
+++ src/NStack.Common/Models/IAssignableId.cs
@@ -0,0 +1,22 @@
+﻿#region header
+// -----------------------------------------------------------------------
+//  <copyright file="IAssignableId.cs" company="Family Bronze, LTD">
+//      © 2013 Mike Grabski and Family Bronze, LTD All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+#endregion
+namespace NStack.Models
+{
+    /// <summary>
+    /// A contract for types that allow the id to be assigned.
+    /// </summary>
+    /// <typeparam name="TId">The type of the id.</typeparam>
+    public interface IAssignableId<in TId>
+    {
+        /// <summary>
+        /// Assigns an identifier.
+        /// </summary>
+        /// <param name="id">The id.</param>
+        void AssignId(TId id);
+    }
+}
